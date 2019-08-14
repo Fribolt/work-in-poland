f13db41bbbd15dfc83ef3cc06ad05ad5834d7f1a
Vlad Pribilnov <vladp@leadsmarket.com>
2019-08-14 20:23:40 +0300

    added routing and main pages

--- work-in-poland/src/app/app-routing.module.ts
+++ work-in-poland/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainPageComponent } from "./pages/main-page/main-page.component";
+import { HowItWorkComponent } from "./pages/how-it-work/how-it-work.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'how-it-work', component: HowItWorkComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- work-in-poland/src/app/app.module.ts
+++ work-in-poland/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HowItWorkComponent } from './pages/how-it-work/how-it-work.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainPageComponent,
+    FooterComponent,
+    HowItWorkComponent
   ],
   imports: [
     BrowserModule,
